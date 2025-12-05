5842d430b21e2b4f913f503095c738b5f66f2bcc
Nguyen Khoi <khoibene@gmail.com>
2025-12-05 22:52:28 +0700

    add requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -96,10 +96,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER", "postgres"),
+        "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
